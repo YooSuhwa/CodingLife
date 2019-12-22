24a876d457dec371d16a2fe64b5e03dc7a33df12
YooSuhwa <usuhwa2@gmail.com>
2019-12-22 19:26:14 +0900

    Create 9663  N-Queen_v2.py

---
+++ Baekjoon_Jungol/9663  N-Queen_v2.py
@@ -0,0 +1,49 @@
+'''
+   @ 2019.12.22 ush
+   * 백준 알고리즘 - 9663 N-Queen (https://www.acmicpc.net/problem/9663)
+   * python (PyPy3)
+
+   * backtracking
+   * Queen은 가로, 세로, 대각선 모두 공격 할 수 있음. 따라서 한 row, col, 오른쪽 위 대각선, 왼쪽 위 대각선 모두 체크해줘야 한다
+   * 1. goRow : 각 row당 하나의 Q만 있도록 재귀함수를 실행
+   * 2. checkOver : 같은 col당 하나의 Q만 있도록
+   * 3. checkDiagR : 오른쪽 위 대각선에 하나의 Q만 있도록
+   * 4. checkDiagL : 왼쪽 위 대각선에 하나의 Q만 있도록
+
+'''
+def check (row, col, N):
+    if checkOver[col] == 1:
+        return -1
+    if checkDiagR[row+col] == 1 :
+        return -1
+    if checkDiagL[row-col+N -1] == 1:
+        return -1
+    return 1
+
+def goRow(row, N):
+    if row == N :
+        global result
+        result += 1
+        return 1
+
+    for col in range(0, N):
+        if check(row, col, N) == 1:
+            checkOver[col] = 1
+            checkDiagR[row+col] = 1
+            checkDiagL[row-col+N-1] = 1
+
+            goRow(row + 1, N)
+
+            checkOver[col] = 0
+            checkDiagR[row + col] = 0
+            checkDiagL[row - col + N - 1] = 0
+    return 1
+
+n = int(input())
+checkOver = [0] * n
+checkDiagR = [0] * (2*n-1)
+checkDiagL = [0] * (2*n-1)
+result = 0
+
+goRow(0, n)
+print(result)
