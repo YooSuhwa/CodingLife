602948be3c4fba1ad19291540284092f779ebf1c
YooSuhwa <usuhwa2@gmail.com>
2019-12-26 16:57:03 +0900

    Update 2003 수들의 합2.py

--- Baekjoon_Jungol/2003 수들의 합2.py
+++ Baekjoon_Jungol/2003 수들의 합2.py
@@ -1,6 +1,6 @@
 '''
    @ ush 2019/12/26
-   * 백준 알고리즘 - 2003 리모컨 (https://www.acmicpc.net/problem/2003)
+   * 백준 알고리즘 - 2003 수들의 합2 (https://www.acmicpc.net/problem/2003)
    * python (PyPy3)
 
    * 수열의 i번째부터 j번째 수까지의 합이 find가 되는 경우의 수 구하기
