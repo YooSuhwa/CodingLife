d42bca3f6c1cc1876fa086561f00110151b63421
YooSuhwa <usuhwa2@gmail.com>
2019-12-28 13:49:38 +0900

    Update 7453 합이 0인 네 정수(MITM).py

--- Baekjoon_Jungol/7453 합이 0인 네 정수(MITM).py
+++ Baekjoon_Jungol/7453 합이 0인 네 정수(MITM).py
@@ -11,6 +11,8 @@
 '''
 
 import sys
+from collections import Counter
+
 n = int(input())
 temp = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 a,b,c,d = zip(*temp)
@@ -25,7 +27,15 @@ for i in range(n):
         teamAB.append(a[i]+b[j])
         teamCD.append(c[i]+d[j])
 
+''' 시간초과 
 for num in teamAB:
     answer += teamCD.count(-num)
+'''
+
+counter = Counter(teamCD)
+#Counter({40: 2, 26: 1, 72: 1, 119: 1, -4: 1, 104: 1, 87: 1, 86: 1, 133: 1, 10: 1, 118: 1, 101: 1, -53: 1, -7: 1, -83: 1, 25: 1, 8: 1, -91: 1, -45: 1, 2: 1, -121: 1, -13: 1, -30: 1, -26: 1, 20: 1, 67: 1, -56: 1, 52: 1, 35: 1, -22: 1, 24: 1, 71: 1, -52: 1, 56: 1, 39: 1})
+print(counter)
+for num in teamAB:
+    answer += counter[-num]
 
 print(answer)
