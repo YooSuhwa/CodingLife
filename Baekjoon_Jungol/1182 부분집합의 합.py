a9174c557daabf83b2886e441633b1565d221cd6
YooSuhwa <usuhwa2@gmail.com>
2019-12-28 13:06:06 +0900

    Update 1182 부분집합의 합.py

--- Baekjoon_Jungol/1182 부분집합의 합.py
+++ Baekjoon_Jungol/1182 부분집합의 합.py
@@ -1,3 +1,11 @@
+'''
+   @ ush 2019/12/28
+   * 백준 알고리즘 - 1182 부분수열의 합 (https://www.acmicpc.net/problem/1182)
+   * python
+
+   * 크기가 양수인 부분수열 중에서 그 수열의 원소를 다 더한 값이 find가 되는 경우의 수를 구하기
+   * bitmask를 이용하여 모든 부분 수열의 합을 구한다
+'''
 n,find = map(int,input().split())
 arr = list(map(int,input().split()))
 totalSet = [0]*(1<<n)
