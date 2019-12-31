0ab8db31a9512456bd02b243ded4fe8b5d75d751
YooSuhwa <usuhwa2@gmail.com>
2019-12-31 15:52:00 +0900

    Update 12851 숨바꼭질2.py

--- Baekjoon_Jungol/12851 숨바꼭질2.py
+++ Baekjoon_Jungol/12851 숨바꼭질2.py
@@ -1,3 +1,15 @@
+'''
+   @ ush 2019/12/31
+   * 백준 알고리즘 - 12851 숨바꼭질4 (https://www.acmicpc.net/problem/12851)
+   * python
+
+   * BFS and DP
+
+   * 위치 start에서 find로 가는 가장 빠른 시간과 그 시간으로 갈 수 있는 방법의 수를 구하는 문제
+   * 가장 빠른 시간을 구하기 : BFS 이용 (dist, visit list)
+   * 방법의 개수 구하기 : DP 이용 (count list)
+
+'''
 from collections import deque
 MAX = 200001
 
@@ -25,7 +37,7 @@ while q :
                 dist[next] = dist[now] + 1
                 count[next] = count[now]
 
-            elif dist[next] == dist[now] + 1:
+            elif dist[next] == dist[now] + 1: #경우의 수 - DP
                 count[next] += count[now]
 
 print(dist[find])
