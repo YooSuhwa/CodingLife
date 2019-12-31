40098db7e426a440feb2880947d991541fcbed0c
YooSuhwa <usuhwa2@gmail.com>
2019-12-31 18:10:23 +0900

    Update 9376 탈옥.py

--- Baekjoon_Jungol/9376 탈옥.py
+++ Baekjoon_Jungol/9376 탈옥.py
@@ -1,3 +1,21 @@
+'''
+   @ 2019.12.31 ush
+   * 백준 알고리즘 - 9376 탈옥 (https://www.acmicpc.net/problem/9376)
+   * python
+
+   * BFS
+   
+   * 두 죄수가 탈옥하기 위해서 열어야 하는 문의 최소 개수를 구하는 문제
+   * 1. 일반적인 BFS는 하나의 시작점 -> 하나의 도착점이다. 
+        따라서 두 죄수를 따로 나누어 BFS를 각각 구한다
+   * 2. 하나의 도착점을 두기 위해서 기존 지도의 테두리를 1칸씩 빈칸으로 확장한다
+   * 3. BFS의 가중치를 계산할 때 
+        문을 지나면 (빈칸>>문) 가중치를 1로 두고 q.push_back
+        문을 지나지 않으면 (빈칸>>빈칸 or 문>>빈칸) 가중치 0으로 두고 q.push_front
+   * 4. 각 죄수에 대한 bfs와 도착점 (보드 밖, 즉 확장한 테두리 point중 하나)에서의 bfs를 각각 구한다.
+   * 5. 위에서 구한 3개의 bfs를 합쳐 답을 구한다.
+    * 문에서 만나는 경우에는, 문은 한번만열어야 하는데 현재 3개의 bfs에서 모두 연 상황이므로 -2 를 해야한다.
+'''
 from collections import deque
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
@@ -41,10 +59,11 @@ for _ in range(t):
     h += 2
     w += 2
     board = ['.' * w] + board + ['.' * w]
-    # bfs0 : from external point (0,0) / bfs1 : prisoner1 / bfs2 : prisoner2
+
+    # bfs0 : from external point (0,0)
     bfs0 = makeBFS(board, 0, 0, h, w)
 
-    # 죄수위치 찾
+    # 죄수위치 찾기
     flag = -1
     for i in range(h):
         for j in range(w):
@@ -58,6 +77,7 @@ for _ in range(t):
                     y2 = j
                     break
 
+    # bfs1 : prisoner1 / bfs2 : prisoner2
     bfs1 = makeBFS(board, x1, y1, h, w)
     bfs2 = makeBFS(board, x2, y2, h, w)
 
