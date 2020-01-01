9c8034e29afbb0091a81c8961178714fab8a5ff7
YooSuhwa <usuhwa2@gmail.com>
2020-01-01 19:31:41 +0900

    Update 9328 열쇠.py

--- Baekjoon_Jungol/9328 열쇠.py
+++ Baekjoon_Jungol/9328 열쇠.py
@@ -18,6 +18,8 @@ visit = [[False]*w for _ in range(h)]
 q.append((1,1))
 visit[1][1]= True
 
+
+
 answer = 0
 while q:
     now = q.popleft()
@@ -31,23 +33,26 @@ while q:
         if visit[newX][newY] :
             continue
 
-        visit[newX][newY] = True
-
         pos = board[newX][newY]
         if pos == '*' : #벽
             continue
+
+        visit[newX][newY] = True
+        if pos == '.' :
+            q.append((newX, newY))
         elif pos == '$' : #문서
             answer += 1
             q.append((newX, newY))
         elif 'A' <= pos <= 'Z': #문 대문자
-            if pos in key : #이미 키 있는 문
+            if pos.lower() in key : #이미 키 있는 문
                 q.append((newX, newY))
             else : #키 없는 문
                 q_alpha[ord(pos)-ord('A')].append((newX, newY))
         elif 'a' <= pos <= 'z' : #열쇠 소문자
             q.append((newX, newY))
             if pos not in key : #처음본 열쇠
-                temp = ord(pos)-ord('A')
+                temp = ord(pos)-ord('a')
+                #print(temp)
                 while q_alpha[temp]:
                     q.append(q_alpha[temp].popleft())
 
