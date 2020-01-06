37b160e6ba2383e2bedb79c650a0371b50443b7c
YooSuhwa <usuhwa2@gmail.com>
2020-01-06 17:08:09 +0900

    Create 15558 점프 게임.py

---
+++ Baekjoon_Jungol/15558 점프 게임.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+n, k = map(int, input().strip().split(' '))
+board = [input() for _ in range(2)]
+dxy = [(0,1), (0,-1), (1,k)]
+
+q = deque()
+dist = [[-1]*n for i in range(2)]
+
+q.append((0,0))
+dist[0][0] = 0
+
+flag = False
+
+while q:
+    (x,y) = q.popleft()
+    for (dx, dy) in dxy:
+        newX = (x + dx)%2
+        newY = y + dy
+
+        if newY >= n :
+            flag = True
+            break
+        if newY <0 :
+            continue
+
+        if board[newX][newY]== '0':#이동할 수 없는 칸
+            continue
+
+        if dist[newX][newY] == -1 :
+            dist[newX][newY] = dist[x][y] + 1
+            q.append((newX, newY))
+
+    if flag :
+        break
+
+if flag:
+    print(1)
+else:
+    print(0)
+    
