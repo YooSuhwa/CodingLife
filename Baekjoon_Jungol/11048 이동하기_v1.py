477882062a96dbc051d50b04e9848204d1aa5500
YooSuhwa <usuhwa2@gmail.com>
2020-01-08 23:02:22 +0900

    Update 11048 이동하기_v1.py

--- Baekjoon_Jungol/11048 이동하기_v1.py
+++ Baekjoon_Jungol/11048 이동하기_v1.py
@@ -6,10 +6,11 @@ m += 1
 map = [[0]+list(map(int,input().split())) for _ in range(n)]
 map = [[0]*m] + map
 n += 1
-dp = [[-1]*m for _ in range(n)]
-
+dp = [[0]*m for _ in range(n)]
 for i in range(1, n):
     for j in range(1, m):
         dp[i][j] = max3(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + map[i][j]
 
-print(dp[n-1][m-1]+1)
+
+print(dp[n-1][m-1])
+
