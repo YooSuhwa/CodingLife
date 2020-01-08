32d4dbada38f32b0df9511d70c5627366ec0fb9d
YooSuhwa <usuhwa2@gmail.com>
2020-01-08 23:03:49 +0900

    Update 11048 이동하기_v2.py

--- Baekjoon_Jungol/11048 이동하기_v2.py
+++ Baekjoon_Jungol/11048 이동하기_v2.py
@@ -1,16 +1,18 @@
 n, m = map(int, input().strip().split(' '))
+m += 1
 map = [[0]+list(map(int,input().split())) for _ in range(n)]
-map = [[0]*(m+1)] + map
-dp = [[0]*(m+1) for _ in range(n+1)]
+map = [[0]*m] + map
+n += 1
+dp = [[0]*m for _ in range(n)]
 
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        if j + 1 <= m and dp[i][j+1] < dp[i][j] + map[i][j+1]:
-            dp[i][j + 1] = dp[i][j] + map[i][j + 1]
-        if i + 1 <=n and dp[i+1][j] < dp[i][j] + map[i+1][j]:
-            dp[i+1][j] = dp[i][j] + map[i+1][j]
-        if i + 1 <= n and j + 1 <= m and dp[i+1][j+1] < dp[i][j] + map[i+1][j+1]:
-            dp[i+1][j + 1] = dp[i][j] + map[i+1][j + 1]
+for i in range(0, n):
+    for j in range(0, m):
+        if j + 1 < m:
+            dp[i][j + 1] = max(dp[i][j + 1], dp[i][j] + map[i][j + 1])
+        if i + 1 <n :
+            dp[i+1][j] = max(dp[i+1][j], dp[i][j] + map[i+1][j])
+        if i + 1 < n and j + 1 < m :
+            dp[i+1][j + 1] = max(dp[i+1][j + 1], dp[i][j] + map[i+1][j + 1])
+print(dp[n-1][m-1])
 
-print(dp[n][m]+1)
 
