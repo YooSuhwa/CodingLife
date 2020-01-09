6b73d5f0ba488c8ba608d32e13c426c8ffceedb3
YooSuhwa <usuhwa2@gmail.com>
2020-01-09 14:32:34 +0900

    Update 11048 이동하기_v4.py

--- Baekjoon_Jungol/11048 이동하기_v4.py
+++ Baekjoon_Jungol/11048 이동하기_v4.py
@@ -1,5 +1,19 @@
+'''
+   @ 2020/01/09 ush
+   * 백준 알고리즘 - 11048 이동하기 (https://www.acmicpc.net/problem/11048)
+   * python
+
+   * dp
+
+   * (1,1) 에서 (n,m)으로 이동하려고 한다. 
+   * 이동할 수 있는 방향은 오른쪽(i,j+1), 왼쪽(i+1, j), 오른쪽아래 대각선(i+1, j+1)이다.
+   * map의 각 칸에 사탕이 주어져 있을 때, 가져올 수 있는 사탕 개수의 최댓값을 구하는 프로그램
+
+   * v4) 재귀함수로 구현하기
+   	* v3의 Bottom-up 이중 포문을 go라는 재귀함수를 통해 Top-down으로 구현
+'''
 import sys
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(1000000) #런타임 에러 원인
 def go(i, j):
     if i <1 or j <1 :
         return 0
