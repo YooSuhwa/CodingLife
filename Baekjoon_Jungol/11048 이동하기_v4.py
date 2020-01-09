d6fa9088884cf0eb5142de2f8b5cd048cdf33436
YooSuhwa <usuhwa2@gmail.com>
2020-01-09 14:31:11 +0900

    Update 11048 이동하기_v4.py

--- Baekjoon_Jungol/11048 이동하기_v4.py
+++ Baekjoon_Jungol/11048 이동하기_v4.py
@@ -1,12 +1,13 @@
-
+import sys
+sys.setrecursionlimit(1000000)
 def go(i, j):
-    if i <0 or j <0 :
-        return
+    if i <1 or j <1 :
+        return 0
     if dp[i][j] >=0 :
         return dp[i][j]
+
     dp[i][j] = max(go(i-1, j), go(i, j-1)) + map[i][j]
     return dp[i][j]
-
 
 n, m = map(int, input().strip().split(' '))
 m += 1
@@ -14,9 +15,5 @@ map = [[0]+list(map(int,input().split())) for _ in range(n)]
 map = [[0]*m] + map
 n += 1
 dp = [[-1]*m for _ in range(n)]
-for i in range(1, n):
-    for j in range(1, m):
-        dp[i][j] = max(dp[i-1][j], dp[i][j-1]) + map[i][j]
 
-
-print(go(n-1,m-1)+1)
+print(go(n-1,m-1))
