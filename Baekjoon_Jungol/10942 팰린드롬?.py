0a2834ee7ff1daa58beed2160e605f8204c47db7
YooSuhwa <usuhwa2@gmail.com>
2020-01-12 23:17:15 +0900

    Update 10942 팰린드롬?.py

--- Baekjoon_Jungol/10942 팰린드롬?.py
+++ Baekjoon_Jungol/10942 팰린드롬?.py
@@ -1,3 +1,19 @@
+'''
+   @ 2020/01/12 ush
+   * 백준 알고리즘 - 10942 팰린드롬? (https://www.acmicpc.net/problem/10942)
+   * python
+
+   * dp
+
+   * 어떤 수열의 부분수열이 팰린드롬인지 확인하는 문제
+
+   * dp[i][j] = arr[i] ~ arr[j]가 팰린드롬이면 1, 아니면 0
+   * 1. 길이가 1일 때 : 반드시 팰린드롬
+   * 2. 길이가 2일 때 : 두수가 같으면 팰린드롬
+   * 3. 그외 (3 이상)
+        arr[i] == arr[j] 이고 arr[i+1]~arr[j-1]이 팰린드롬이여야 한다.
+        즉, arr[i] == arr[j] and dp[i+1][j-1] = 1
+'''
 def go (start, end):
     if start == end : # 한글자
         return 1
