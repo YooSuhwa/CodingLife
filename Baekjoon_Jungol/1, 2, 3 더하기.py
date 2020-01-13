23ac211f7170f1a91ef7c1b6a4f4c2f3a16b36a8
YooSuhwa <usuhwa2@gmail.com>
2020-01-13 14:39:04 +0900

    Create 1, 2, 3 더하기.py

---
+++ Baekjoon_Jungol/1, 2, 3 더하기.py
@@ -0,0 +1,16 @@
+testcase = int(input())
+
+for _ in range(testcase):
+    n = int(input())
+    dp = [0] * (n+1)
+
+    size = 3
+    seq = (1,2,3)
+    dp[0] = 1
+
+    for j in range(1, n + 1):
+        for i in range(0, size):
+            if j-seq[i] >= 0:
+                dp[j] += dp[j - seq[i]]
+
+    print(dp[n])
