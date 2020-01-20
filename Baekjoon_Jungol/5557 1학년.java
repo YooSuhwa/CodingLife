17883518b34000b8ae6627606a4dd880f60b2b78
YooSuhwa <usuhwa2@gmail.com>
2020-01-20 15:48:18 +0900

    Update 5557 1학년.java

--- Baekjoon_Jungol/5557 1학년.java
+++ Baekjoon_Jungol/5557 1학년.java
@@ -1,36 +1,33 @@
-package Ush;
-import java.util.Scanner;
+import java.util.*;
 public class Main {
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			
-			int n = sc.nextInt();			
-		
-			int[] arr = new int [n];
-			for (int i = 0; i < n; i++) {
-				arr[i] = sc.nextInt();
-			}
-			n--;
-			
-			int limit = 20;
-			limit++;
-
-			//i까지의 수를 사용해서 j를 만드는 방법의 수 
-			int[][] dp = new int [n][limit];
-			
-			dp[0][arr[0]] = 1;
-			for (int i=1; i<n; i++) {
-	            for (int j=0; j< limit; j++) {
-	                if (j-arr[i] > -1) {
-	                    dp[i][j] += dp[i-1][j-arr[i]];
-	                }
-	                if (j+arr[i] < limit) {
-	                    dp[i][j] += dp[i-1][j+arr[i]];
-	                }
-	            }
-	        }
-
-			System.out.println(dp[n-1][arr[n]]);	
-		}
+    static int[][] d;
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        
+        n -= 1;
+        
+        d = new int [n][21];
+        d[0][arr[0]] = 1;
+        
+        for (int i=1; i<n; i++) {
+            for (int j=0; j<=20; j++) {
+                if (j-arr[i] >= 0) {
+                    d[i][j] += d[i-1][j-arr[i]];
+                }
+                if (j+arr[i] <= 20) {
+                    d[i][j] += d[i-1][j+arr[i]];
+                }
+            }
+        }
+        System.out.println(d[n-1][arr[n]]);
+    }
 }
+
+
 
