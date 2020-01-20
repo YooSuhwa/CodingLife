3e6a4221038741ecb87eb0675b753e277d1f5ba2
YooSuhwa <usuhwa2@gmail.com>
2020-01-20 15:48:55 +0900

    Update 5557 1학년.java

--- Baekjoon_Jungol/5557 1학년.java
+++ Baekjoon_Jungol/5557 1학년.java
@@ -1,6 +1,5 @@
 import java.util.*;
 public class Main {
-    static long[][] d;
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         
@@ -10,9 +9,11 @@ public class Main {
             arr[i] = sc.nextInt();
         }
         
-        n -= 1;
+        int limit = 20;
+        limit++;
+        n --;
         
-        d = new long [n][21];
+        long[][] d = new long [n][limit];
         d[0][arr[0]] = 1;
         
         for (int i=1; i<n; i++) {
